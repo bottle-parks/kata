974be38681e5e4571c4bd488fe41c10f350c4ae1
JayPark7821 <pjhyun1990@gmail.com>
2023-01-05 18:50:12 +0900

    test 차량 이름 validation 테스트 추가.

--- src/test/java/com/bottleparks/kata/carracing/pjh/CarTest.java
+++ src/test/java/com/bottleparks/kata/carracing/pjh/CarTest.java
@@ -1,5 +1,7 @@
 package com.bottleparks.kata.carracing.pjh;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
@@ -7,5 +9,11 @@ public class CarTest {
 	@Test
 	void canCreateCarWithName() throws Exception {
 		Car car = new Car("test");
+	}
+
+	@Test
+	void 차량이름의_길이가_0_이거나_5보다_크면_ERROR () throws Exception {
+		Assertions.assertThatThrownBy(() -> new Car("")).isExactlyInstanceOf(IllegalArgumentException.class);
+		Assertions.assertThatThrownBy(() -> new Car("1256fe")).isExactlyInstanceOf(IllegalArgumentException.class);
 	}
 }
