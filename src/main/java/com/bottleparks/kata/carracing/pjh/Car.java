0cde462065aa1568e7cc882f520a1c0a427c6d9b
JayPark7821 <pjhyun1990@gmail.com>
2023-01-05 18:44:41 +0900

    feat can create car with name

---
+++ src/main/java/com/bottleparks/kata/carracing/pjh/Car.java
@@ -0,0 +1,10 @@
+package com.bottleparks.kata.carracing.pjh;
+
+public class Car {
+
+	private final String name;
+
+	public Car(String name) {
+		this.name = name;
+	}
+}
---
+++ src/test/java/com/bottleparks/kata/carracing/pjh/CarTest.java
@@ -0,0 +1,11 @@
+package com.bottleparks.kata.carracing.pjh;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+	@Test
+	void canCreateCarWithName() throws Exception {
+		Car car = new Car("test");
+	}
+}
