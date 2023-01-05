e15f83976b0bca41d15df44313cc8e84e2acb6f1
JayPark7821 <pjhyun1990@gmail.com>
2023-01-05 18:52:13 +0900

    feat 차량명 길이 validation 추가.

--- src/main/java/com/bottleparks/kata/carracing/pjh/Car.java
+++ src/main/java/com/bottleparks/kata/carracing/pjh/Car.java
@@ -5,6 +5,10 @@ public class Car {
 	private final String name;
 
 	public Car(String name) {
+		if(name.length() == 0 || name.length() > 5) {
+			throw new IllegalArgumentException("자동차 이름은 0~5자만 허용 합니다.");
+		}
+
 		this.name = name;
 	}
 }
