0cc58ec55cbbaffc726a56fdb5f62ea6b5336ef3
cyPark95 <cas15987@gmail.com>
2023-01-27 16:14:44 +0900

    feat: View

---
+++ src/main/java/com/bottleparks/kata/carracing/pcy/utils/constants/OutputConstant.java
@@ -0,0 +1,9 @@
+package com.bottleparks.kata.carracing.pcy.utils.constants;
+
+public class OutputConstant {
+
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String PLAY_RESULT = "실행 결과";
+    public static final String FINAL_WINNER = "가 최종 우승했습니다.";
+}
